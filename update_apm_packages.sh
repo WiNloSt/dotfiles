2ec3015d37b0dd82a2c9506232e8db224ed11bdc
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-05-30 22:03:16 +0700

    Support update atom settings in both OS X and Ubuntu

--- update_apm_packages.sh
+++ update_apm_packages.sh
@@ -3,9 +3,15 @@
 apm list -ib > .atom/packages.list
 
 # copy all config files
-find ~/.atom -type f -name "*.cson" -d 1 | xargs -I {} cp {} .atom/
-find ~/.atom -type f -name "*.coffee" -d 1 | xargs -I {} cp {} .atom/
-find ~/.atom -type f -name "*.less" -d 1 | xargs -I {} cp {} .atom/
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  find ~/.atom -maxdepth 1 -type f -name "*.cson"  | xargs -I {} cp {} .atom/
+  find ~/.atom -maxdepth 1 -type f -name "*.coffee" | xargs -I {} cp {} .atom/
+  find ~/.atom -maxdepth 1 -type f -name "*.less" | xargs -I {} cp {} .atom/
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  find ~/.atom -type f -d 1 -name "*.cson" | xargs -I {} cp {} .atom/
+  find ~/.atom -type f -d 1 -name "*.coffee" | xargs -I {} cp {} .atom/
+  find ~/.atom -type f -d 1 -name "*.less" | xargs -I {} cp {} .atom/
+fi
 
 # delete projecct manager config since it should be machine specific
 rm .atom/projects.cson

ebc27ee2bf75590be23c9da319c7419759e6c0f3
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-05-30 22:15:31 +0700

    Filter projects.cson out instead of removing it later

--- update_apm_packages.sh
+++ update_apm_packages.sh
@@ -2,16 +2,13 @@
 
 apm list -ib > .atom/packages.list
 
-# copy all config files
+# copy all config files except projects.cson (project manager) since each machine will have different project settings
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  find ~/.atom -maxdepth 1 -type f -name "*.cson"  | xargs -I {} cp {} .atom/
+  find ~/.atom -maxdepth 1 -type f -name "*.cson" | grep projects.cson --invert-match | xargs -I {} cp {} .atom/
   find ~/.atom -maxdepth 1 -type f -name "*.coffee" | xargs -I {} cp {} .atom/
   find ~/.atom -maxdepth 1 -type f -name "*.less" | xargs -I {} cp {} .atom/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  find ~/.atom -type f -d 1 -name "*.cson" | xargs -I {} cp {} .atom/
+  find ~/.atom -type f -d 1 -name "*.cson" | grep projects.cson --invert-match | xargs -I {} cp {} .atom/
   find ~/.atom -type f -d 1 -name "*.coffee" | xargs -I {} cp {} .atom/
   find ~/.atom -type f -d 1 -name "*.less" | xargs -I {} cp {} .atom/
 fi
-
-# delete projecct manager config since it should be machine specific
-rm .atom/projects.cson

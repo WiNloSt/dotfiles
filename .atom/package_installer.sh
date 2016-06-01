28434c6ebb56a21f402174821fe5a250cb4400fc
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-06-01 20:49:28 +0700

    Enhance Atom package installer
    - install only missing packages
    - support installing packages from Github

--- .atom/package_installer.sh
+++ .atom/package_installer.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-apm install --packages-file packages.list
+while read packageWithVersion; do
+  if [[ -z $packageWithVersion ]]; then
+    isGithubPackage=true
+  fi
+
+  package=
+  if [[ $packageWithVersion =~ ^([a-zA-Z0-9-]+) ]]
+  then
+    package=${BASH_REMATCH[0]}
+  fi
+
+  if [[ $isGithubPackage && -n $package ]]; then
+    # install Github packages
+    apm upgrade -c false $package
+  elif [[ -n $package ]]; then
+    # install missing packages
+    if [[ ! -d "$HOME/.atom/packages/$package" ]]
+    then
+      apm install $package
+    fi
+  fi
+done <packages.list

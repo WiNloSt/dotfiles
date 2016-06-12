f96e450608901b3c8e38c7d5535db944d58513b6
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-06-12 14:14:55 +0700

    Refactor bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,28 +14,28 @@ function doIt() {
   --exclude "update_apm_packages.sh" \
   -avh --no-perms . ~;
   source ~/.bash_profile;
-
-	read -p "Do you want to update Atom packages? This will take a while. (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		installAtomPackages;
-	fi;
 }
 
 function installAtomPackages() {
 	# install Atom packages
 	cd .atom
-	source package_installer.sh;
+	source package_installer.sh
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt;
+	doIt
+  installAtomPackages
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-  echo "";
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-  doIt;
-  fi;
-fi;
-unset doIt;
-unset installAtomPackages;
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+  read -p "Do you want to update Atom packages? This will take a while. (y/n) " -n 1
+  echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		installAtomPackages
+	fi
+fi
+unset doIt
+unset installAtomPackages

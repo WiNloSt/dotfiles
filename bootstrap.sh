bae02067465a39992259eb5fa7342ba45517eadc
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-06-12 14:14:55 +0700

    Refactor bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,30 +7,30 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "update_apm_packages.sh" \
-		--exclude ".bashshrc.windows" -avh --no-perms . ~;
-	source ~/.bash_profile;
-
-	read -p "Do you want to update Atom packages? This will take a while. (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		installAtomPackages;
-	fi;
+		--exclude ".bashshrc.windows" -avh --no-perms . ~
+	source ~/.bash_profile
 }
 
 function installAtomPackages() {
 	# install Atom packages
 	cd .atom
-	source package_installer.sh;
+	source package_installer.sh
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	doIt
+  installAtomPackages
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
-unset installAtomPackages;
+		doIt
+	fi
+  read -p "Do you want to update Atom packages? This will take a while. (y/n) " -n 1
+  echo ""
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		installAtomPackages
+	fi
+fi
+unset doIt
+unset installAtomPackages
